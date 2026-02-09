821bd006311710ffcdc398658812b41c06ec9cd8
Yoav <yoav.h@campus.technion.ac.il>
2026-02-09 23:59:40 +0200

    minor change and config for a refined max_rank scan.

--- pbs_experimenting/param_generator.py
+++ pbs_experimenting/param_generator.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
         print("Usage: python param_generator.py <config.yaml>")
         sys.exit(1)
 
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
 
     combos = generate_job_parameters(config)
